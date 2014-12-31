72e564f8af32a9a8502cdd2b1244d562fbce7a94
Brian R. Jackson <brian@jaxzin.com>
2014-12-30 23:17:50 -0500

    Added danbury.io meetup feed.

--- app/scripts/controllers/meetup.js
+++ app/scripts/controllers/meetup.js
@@ -11,6 +11,10 @@ angular.module('nhvioApp')
     var feeds = [
       {
         "eventPrefix": "",
+        "meetupUrl": "http://api.meetup.com/2/events?status=upcoming&order=time&limited_events=False&group_urlname=danburyio&desc=false&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=13965389&sig=e5a06bd53550449466557602e1dcadcaf76d5465"
+      },
+      {
+        "eventPrefix": "",
         "meetupUrl": "http://api.meetup.com/2/events?callback=JSON_CALLBACK&status=upcoming&order=time&limited_events=False&group_urlname=Danbury-Hackerspace&desc=false&offset=0&photo-host=public&format=json&page=20&fields=&sig_id=13965389&sig=08b18745a5e2cea862196e5f26754eec1b13408b"
       }
     ];
