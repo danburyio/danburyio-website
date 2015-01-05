36397dd62c8ff4f014b08366414da9dfe5134ca2
Brian R. Jackson <brian@jaxzin.com>
2015-01-04 21:13:15 -0500

    Added test for CompanyEditCtrl.

--- test/spec/controllers/companyEdit.js
+++ test/spec/controllers/companyEdit.js
@@ -5,18 +5,43 @@ describe('Controller: CompanyEditCtrl', function () {
   // load the controller's module
   beforeEach(module('nhvioApp'));
 
-  var CompanyeditCtrl,
-    scope;
+  var CompanyEditCtrl,
+    scope,
+    httpBackend,
+    controller;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+    controller = $controller;
+
+    httpBackend = $httpBackend;
+
     scope = $rootScope.$new();
-    CompanyeditCtrl = $controller('CompanyEditCtrl', {
-      $scope: scope
-    });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  afterEach(function() {
+    httpBackend.flush();
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
   });
+
+
+  it('should create a new company', function () {
+    httpBackend.expect('GET', '/api/users/me')
+    .respond({
+    });
+    httpBackend.expect('GET', '/api/languages')
+    .respond({
+    });
+    httpBackend.expect('GET', '/api/users')
+    .respond({
+    });
+
+    CompanyEditCtrl = controller('CompanyEditCtrl', {
+      $scope: scope
+    });
+
+    expect(scope.company.isNew).toBe(true);
+  });
+
 });
